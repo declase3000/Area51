069dbc64c8512390e4c1c76e7359b05bcf3ad6bd
declase3000 <declase3000@gmail.com>
2018-08-14 13:16:37 -0500

    subiendo archivos

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -10,6 +10,37 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+           // string hiddenWord="palabra";
+           string hiddenWord = Console.ReadLine();
+
+            string showWord="";
+
+           Console.Clear();
+          //  if (hiddenWord=="hola"){
+           //     Console.WriteLine("Chau");
+
+          //  } 
+
+           // for (int i=0;i<3; i++){
+             //   Console.WriteLine(hiddenWord);
+             //   }
+
+            for (int i=0; i<hiddenWord.Length;i++){
+
+               // Console.WriteLine(hiddenWord);
+               showWord +='*';
+            }
+
+
+
+
+
+
+            Console.WriteLine(hiddenWord);
+            
+           
+                
+            Console.Read();
         }
     }
 }
